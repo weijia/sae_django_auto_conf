0437c019bed4f8ab239f3c326fbb7dc9c6b8a87f
Richard <richardwangwang@gmail.com>
2014-03-26 11:29:24 +0800

    Adding web manage app.

---
+++ libs/webmanage/urls.py
@@ -0,0 +1,11 @@
+#from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+import os
+
+urlpatterns = patterns('',
+    url(r'^cmd/$', 'webmanage.views.cmd'),
+    url(r'^syncdb/$', 'webmanage.views.index'),
+    url(r'^django_version/$', 'webmanage.views.version'),
+    url(r'^create_admin_user/$', 'webmanage.views.create_admin')
+)
